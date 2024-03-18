782e2b8b2ac1077311c4ae086cbc4d3aba4fb6b8
Rasmus Jensen <raserasedj@gmail.com>
2024-03-18 10:56:06 +0100

    [update] logging init

--- app/logging.py
+++ app/logging.py
@@ -1,2 +1,16 @@
 import logging
+import psutil
 
+
+def monteCarloLogger():
+    logging.basicConfig(filename="app.log", 
+                        level=logging.INFO, 
+                        format='%(asctime)s - CPU load: %(cpu_load)s - RAM load: %(ram_load)s - Used RAM: %(used_ram)s- %(message)s'
+                        )
+    
+def getCPUandRAMLoad():
+    CPULoad = psutil.cpu_percent()
+    RAMLoad = psutil.virtual_memory().percent
+    availableRAM = psutil.virtual_memory().available
+    totalLoad = [CPULoad, RAMLoad, availableRAM]
+    return totalLoad
