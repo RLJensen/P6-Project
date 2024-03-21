ec9c1eb9d65ebbcf309b5f64223b2ce2393cf92f
RaJensen120 <70698966+RaJensen120@users.noreply.github.com>
2024-03-21 12:15:06 +0100

    new commit

--- app/main.py
+++ app/main.py
@@ -1,37 +1,20 @@
 import monteCarloAlgo
 import logging
-import EncryptionWorkload
+import whisperWorkload
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     logging.info("Starting program")
     
-    # # Number of random points to generate
-    # num_points = 1
+     # Number of random points to generate
+    num_points = 1
 
-    # # Estimating pi
-    # pi_approximation = monteCarloAlgo.estimate_pi(num_points)
-    # print("Approximation of pi using Monte Carlo method:", pi_approximation[0])
-    # logObjcets = pi_approximation[1]
-    # for obj in logObjcets:
-    #     logging.info(obj)
-
-    EncryptionWorkload.primefiller()
-    EncryptionWorkload.setkeys()
-    print("Public key: ", EncryptionWorkload.public_key)
-    print("Private key: ", EncryptionWorkload.private_key)
-    message = "Hello World!"
-	# Uncomment below for manual input
-	# message = input("Enter the message\n")
-	# Calling the encoding function
-    coded = EncryptionWorkload.encoder(message)
-
-    print("Initial message:")
-    print(message)
-    print("\n\nThe encoded message(encrypted by public key)\n")
-    print(''.join(str(p) for p in coded))
-    print("\n\nThe decoded message(decrypted by public key)\n")
-    print(''.join(str(p) for p in EncryptionWorkload.decoder(coded)))
+    # Estimating pi
+    pi_approximation = monteCarloAlgo.estimate_pi(num_points)
+    print("Approximation of pi using Monte Carlo method:", pi_approximation[0])
+    logObjcets = pi_approximation[1]
+    for obj in logObjcets:
+        logging.info(obj)
 
 
 
