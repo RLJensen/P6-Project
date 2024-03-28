dd27a4c346a22d910b31ecf7fb5362a63cc6d1ac
Rasmus Jensen <raserasedj@gmail.com>
2024-03-28 10:45:59 +0100

    [update] model dict

--- app/whisperWorkload/whisperWorkload.py
+++ app/whisperWorkload/whisperWorkload.py
@@ -1,12 +1,18 @@
 import whisper
+import logging
 import logger
 from googlesearch import search
+
+def startWorkload():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info("Starting whisper workload")
+    loadModel()
 
 def loadModel():
     logs = []
     currentLog = logger.getCPUandRAMLoad(logger.getLoad())
     logs.append(currentLog)
-    model = whisper.load_model("tiny")
+    model = whisper.load_model("tiny",None,download_root="/models")
     currentLog = logger.getCPUandRAMLoad(logger.getLoad())
     logs.append(currentLog)
     result = model.transcribe("whisperWorkload/test.mp3",fp16=False)
@@ -20,3 +26,6 @@ def loadModel():
 def performSearch(query):
     result = search(query,10,"en",None,False,10,5)
     return result
+
+if __name__ == "__whisperWorkload__":
+    startWorkload()
