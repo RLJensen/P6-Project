07b248348bd47c490acbdb6bfb0aae3f4f680185
Casper Christensen <caschr21@student.aau.dk>
2024-05-24 13:20:56 +0200

    [update] remove logging loki tests from tsp and mc

--- app/TSP/test_tspUnitTests.py
+++ app/TSP/test_tspUnitTests.py
@@ -13,7 +13,6 @@ class TestTSP(unittest.TestCase):
         filename = 'TSP/cities.txt'
         num_cities = 10
         cities = tspAlgorithm.read_city_coordinates(filename, num_cities)
-        print(cities)
         self.assertEqual(len(cities), num_cities)
 
     def test_generate_initial_population(self):
@@ -58,86 +57,6 @@ class TestTSP(unittest.TestCase):
         num_generations = 3
         result = tspAlgorithm.genetic_algorithm(self.cities)
         self.assertEqual(len(result), 2)
-    
-    @patch('tspAlgorithm.logging.getLogger')
-    @patch('tspAlgorithm.load_dotenv')
-    @patch('tspAlgorithm.logging_loki.LokiHandler')
-    @patch('tspAlgorithm.hostname', 'mock-hostname')
-    @patch('tspAlgorithm.workload_type', 'mock-workload')
-    @patch('tspAlgorithm.uuid', 'mock-uuid')
-    def test_setup_logger(self, mock_LokiHandler, mock_load_dotenv, mock_getLogger):
-        # Mock the environment variables
-        mock_env = {
-            'GRAFANACLOUD_URL': 'http://mock-url',
-            'GRAFANACLOUD_USERNAME': 'mock-user',
-            'GRAFANACLOUD_PASSWORD': 'mock-pass'
-        }
-        
-        with patch.dict(os.environ, mock_env):
-            # Mock the logger and its methods
-            mock_logger = MagicMock()
-            mock_getLogger.return_value = mock_logger
-
-            # Mock the LokiHandler instance
-            mock_handler = MagicMock()
-            mock_LokiHandler.return_value = mock_handler
-
-            # Call the setup_logger function
-            setup_logger()
-
-            # Check logger level is set to INFO
-            mock_logger.setLevel.assert_called_with(logging.INFO)
-
-            # Ensure load_dotenv was called
-            mock_load_dotenv.assert_called_once()
-
-            # Check if existing handlers were cleared
-            mock_logger.handlers.clear.assert_called_once()
-
-            # Check LokiHandler was created with correct parameters
-            mock_LokiHandler.assert_called_once_with(
-                url='http://mock-url',
-                tags={
-                    "application": "Workload",
-                    "host": 'mock-hostname',
-                    "workload":'mock-workload',
-                    "affinity": "worker3",
-                    "uuid": 'mock-uuid'
-                },
-                auth=('mock-user', 'mock-pass'),
-                version="1"
-            )
-
-            # Check the handler was added to the logger
-            mock_logger.addHandler.assert_called_once_with(mock_handler)
-
-            # Verify formatter was set correctly
-            mock_handler.setFormatter.assert_called_once()
-            formatter_arg = mock_handler.setFormatter.call_args[0][0]
-            self.assertIsInstance(formatter_arg, logging.Formatter)
-            self.assertEqual(formatter_arg._fmt, '%(asctime)s - %(levelname)s - %(message)s')
-
-    @patch('tspAlgorithm.logging.getLogger')
-    @patch('tspAlgorithm.load_dotenv')
-    @patch('tspAlgorithm.logging_loki.LokiHandler', side_effect=Exception('mock error'))
-    def test_setup_logger_exception(self, mock_LokiHandler, mock_load_dotenv, mock_getLogger):
-        # Mock the environment variables
-        mock_env = {
-            'GRAFANACLOUD_URL': 'http://mock-url',
-            'GRAFANACLOUD_USERNAME': 'mock-user',
-            'GRAFANACLOUD_PASSWORD': 'mock-pass'
-        }
-
-        with patch.dict(os.environ, mock_env):
-            # Mock the logger and its methods
-            mock_logger = MagicMock()
-            mock_getLogger.return_value = mock_logger
-
-            # Expect the setup_logger to raise an exception
-            with self.assertRaises(Exception) as context:
-                setup_logger()
-
-            self.assertEqual(str(context.exception), 'mock error')
 
 if __name__ == '__main__':
     unittest.main()
